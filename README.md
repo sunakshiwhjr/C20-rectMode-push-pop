3af33f17b5ac758db2262d0d5887c13020e9d5e3
sunakshiwhjr <77574525+sunakshiwhjr@users.noreply.github.com>
2021-07-17 08:48:39 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# C20-rectMode-push-pop
+# C20-Student-boilerplate-code
---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+function setup()
+{
+   createCanvas(400, 400);
+
+}
+
+
+function draw()
+{
+    background("purple");
+    push();
+    fill("Pink");
+    rectMode(CENTER);
+
+    rect(200, 200, 60, 40);
+    pop();
+    //fill("Yellow");
+    rect( 300, 300, 50, 60);
+  
+ 
+ 
+    
+
+}
